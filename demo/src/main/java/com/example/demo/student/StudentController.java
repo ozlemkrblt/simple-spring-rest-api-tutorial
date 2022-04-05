b2df92cf319e591df8da9e00dc79137fe2bf1b45
ozlemkrblt <ozlemkarabulut99@gmail.com>
2022-04-05 16:31:39 +0300

    create simple business layer

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,24 +1,25 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
-//All of the resources for API
+/*This is the class that has all of the resources for API */
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
     private final StudentService studentService;
 
+    @Autowired //dependency injection
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
-   // @GetMapping
-   // public List<Student> getStudents(){
-
-    //}
+   @GetMapping
+   public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
 
 }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,14 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service //dependency injection
 public class StudentService {
     @GetMapping
     public List<Student> getStudents() {
