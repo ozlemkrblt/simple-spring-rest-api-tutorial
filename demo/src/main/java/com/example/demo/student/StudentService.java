c54689e17d6746fe223ed22025a0f1e9054f7749
ozlemkrblt <ozlemkarabulut99@gmail.com>
2022-04-05 16:24:45 +0300

    create simple business layer

---
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Özlem",
+                        "ozlemkarabulut99@gmail.com",
+                        LocalDate.of(1999, Month.FEBRUARY, 24),
+                        21
+                )
+        );
+    }
+}
